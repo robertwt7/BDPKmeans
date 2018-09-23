cda5042f550dac9b37a570c23d753e25481fe577
Robert Tirtasentana <roberttirtasentana@192-168-1-100.tpgi.com.au>
2018-09-23 15:23:02 +1000

    Project Base

---
+++ src/main/java/au/edu/rmit/bdp/customdata/App.java
@@ -0,0 +1,15 @@
+package au.edu.rmit.bdp.customdata;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
---
+++ src/test/java/au/edu/rmit/bdp/customdata/AppTest.java
@@ -0,0 +1,20 @@
+package au.edu.rmit.bdp.customdata;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
